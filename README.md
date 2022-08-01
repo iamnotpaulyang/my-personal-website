e5503a029402af969641decd2cb10e3ea07e216e
Paul Yang <paulyang00@gmail.com>
2022-08-01 02:32:53 -0400

    last draft

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # My Personal Website
+The purpose of this project is to help you get more context about me.
+The first webpage provides a link to my personal website, as well as having all social media links that relate towards me.
+The second webpage is a slide page which provides personal information about myself and my future incoming projects to be announced, along with a get in touch form where you can ask me anything.
+
+Sources used:
+All icons for social media links were used from: https://www.flaticon.com/
+Black and white notepad and Macbook and iPhone background was used from: https://www.codementor.io/learn-programming/12-important-things-to-include-in-web-dev-portfolios
+Coming soon projects background was used from: https://www.maxpixel.net/Coming-Soon-Technology-Coming-Under-Construction-2461832
+Get in touch background was used from: https://www.vectorstock.com/royalty-free-vector/white-blue-technology-background-vector-20643924
+
+AUTHORS: PAUL YANG
--- script.js
+++ script.js
@@ -1,7 +1,88 @@
-// const h2 = document.createElement("h2");
-// h2.textContent = "";
-// document.querySelector("body").appendChild(h2);
+const h2 = document.createElement("h2");
+h2.textContent = "";
+document.querySelector("body").appendChild(h2);
 const navScroll = (section) => {
     const location = document.getElementById(section)
     location.scrollIntoView({behavior:"smooth", block: "end"})
 }
+var fields = {};
+document.addEventListener("DOMContentLoaded", function() {
+    fields.firstName = document.getElementById('firstName');
+    fields.lastName = document.getElementById('lastName');
+    fields.email = document.getElementById('email');
+    fields.updates = document.getElementById('updates');
+    fields.question = document.getElementById('question');
+   })
+
+function isNotEmpty(value) {
+    if (value == null || typeof value == 'undefined' ) return false;
+    return (value.length > 0);
+   }
+
+function isEmail(email) {
+    let regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+    return regex.test(String(email).toLowerCase());
+   }
+
+function fieldValidation(field, validationFunction) {
+    if (field == null) return false;
+   
+    let isFieldValid = validationFunction(field.value)
+    if (!isFieldValid) {
+    field.className = 'placeholderRed';
+    } else {
+    field.className = '';
+    }
+   
+    return isFieldValid;
+   }
+
+   function isValid() {
+    var valid = true;
+    
+    valid &= fieldValidation(fields.firstName, isNotEmpty);
+    valid &= fieldValidation(fields.lastName, isNotEmpty);
+    valid &= fieldValidation(fields.email, isEmail);
+    valid &= fieldValidation(fields.question, isNotEmpty);
+    
+   return valid;
+   }
+
+   class User {
+    constructor(firstName, lastName, email, updates, question) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.updates = updates;
+    this.question = question;
+    }
+   }
+
+   function sendContact() {
+    if (isValid()) {
+        let usr = new User(firstName.value, lastName.value, email.value, updates.checked, question.value);
+        
+        alert(`${usr.firstName} thank you for your submission.`)
+    
+    }else {
+        alert ("There was an error")
+    }
+}
+
+function startLoader() {
+    const loader = document.querySelector(".loader");
+  
+    setTimeout(function() {
+      loader.style.right = "0";
+    }, 1000);
+  
+    window.addEventListener("DOMContentLoaded", function(event) {
+      setTimeout(function() {
+        loader.style.left = "100%";
+        document.body.classList.remove("loading");
+      }, 1500);
+    });
+  }
+  
+  startLoader();
+  
